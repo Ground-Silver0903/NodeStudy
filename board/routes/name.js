6c2497a818861a87102f54626bbae73eb61fd547
Ground-Silver0903 <kyn07077@gmail.com>
2017-05-04 23:21:07 +0900

    node comeback

--- board/routes/name.js
+++ board/routes/name.js
@@ -7,23 +7,31 @@ module.exports = (router, User) => {
     });
 
     router.post('/dbIn', function(req,res){
-        var user = new User({
-            name : req.body.user_name,
-            age : req.body.user_age,
-            school : req.body.user_school,
-            friend : req.body.user_friend
-        }); 
 
-        user.save(function(err){
-           if(err){
-                console.log('error!');
-           }else{
-               res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-               res.end('Name : ' + req.body.user_name + '\n age : ' + req.body.user_age + '\nschool : ' + req.body.user_school + '\nfriend : ' + req.body.user_friend)
-           
-    }
-          
+        res.render('login',{
+           name : req.body.user_name,
+           age : req.body.user_age,
+           school : req.body.user_school,
+           friend : req.body.user_friend 
         });
+
+    //     var user = new User({
+    //         name : req.body.user_name,
+    //         age : req.body.user_age,
+    //         school : req.body.user_school,
+    //         friend : req.body.user_friend
+    //     }); 
+
+    //     user.save(function(err){
+    //        if(err){
+    //             console.log('error!');
+    //        }else{
+    //            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    //            res.end('Name : ' + req.body.user_name + '\n age : ' + req.body.user_age + '\nschool : ' + req.body.user_school + '\nfriend : ' + req.body.user_friend)
+           
+    // // }
+          
+    //     });
     })
 return router;
 
